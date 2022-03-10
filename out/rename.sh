37671979d0288b3070285b8e35bedecd61b0d276
InconsolableCellist <sadfklj@sdlkfj.com>
2022-03-10 15:31:45 -0700

    Initial check-in

---
+++ out/rename.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+cp c64psu-F.Cu.gbr renamed/c64psu.GTL
+cp c64psu-B.Cu.gbr renamed/c64psu.GBL
+cp c64psu-F.Mask.gbr renamed/c64psu.GTS
+cp c64psu-B.Mask.gbr renamed/c64psu.GBS
+cp c64psu-F.SilkS.gbr renamed/c64psu.GTO
+cp c64psu-B.SilkS.gbr renamed/c64psu.GBO
+cp c64psu-Edge.Cuts.gbr renamed/c64psu.GKO
+cp c64psu.drl renamed/c64psu.DRL
+
